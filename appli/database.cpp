da1b7139c88d45d3883368898515172385576ce8
JeresB <boiselet.jeremy@gmail.com>
2017-05-16 11:33:44 +0200

    Première ebauche de la classe database

---
+++ appli/database.cpp
@@ -0,0 +1,10 @@
+#include "database.hpp"
+#include <QDebug>
+
+QT_USE_NAMESPACE
+
+//! [constructor]
+Database::Database(QObject *parent) : QObject(parent) {
+  
+}
+//! [constructor]
---
+++ appli/database.hpp
@@ -0,0 +1,21 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QObject>
+
+class Database : public QObject {
+    Q_OBJECT
+public:
+    explicit Database(QObject *parent = Q_NULLPTR);
+
+Q_SIGNALS:
+
+
+private Q_SLOTS:
+
+
+private:
+
+};
+
+#endif // DATABASE_H
