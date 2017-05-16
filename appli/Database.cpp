342775087d49bf53343b052634b896c2234b6c52
JeresB <boiselet.jeremy@gmail.com>
2017-05-16 17:25:01 +0200

    test11

--- appli/Database.cpp
+++ appli/Database.cpp
@@ -32,7 +32,7 @@ int Database::requete(QString requete) {
       for(int i=0; i < record.count(); i++) {
         QString colonne = record.fieldName(i);
         QVariant value = query.value(i);
-
+        QJsonValue json;
         if (value.canConvert<int>()) {
           QJsonValue json = value.toInt();
         } else if (value.canConvert<QString>()) {
