f13b13b65b0a34124aec756ebc77ab7e271300e6
JeresB <boiselet.jeremy@gmail.com>
2017-05-16 12:27:18 +0200

    correction requete

--- appli/main.cpp
+++ appli/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 
   Database *livinglab = new Database;
 
-  livinglab::requete("SELECT * FROM chambre");
+  livinglab->requete("SELECT * FROM chambre");
 
   return appli.exec();
 }
