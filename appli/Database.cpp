0d86fec3b7f377a40ca416ca4486feefae3203fe
JeresB <boiselet.jeremy@gmail.com>
2017-05-16 17:47:48 +0200

    test25

--- appli/Database.cpp
+++ appli/Database.cpp
@@ -27,25 +27,25 @@ QString Database::requete(QString requete) {
     query  = db.exec(requete);
     record = query.record();
     qDebug() << "Selection réussi :)";
-    //
-    // QJsonDocument  json;
-    // QJsonArray     recordsArray;
 
-  //   while(query.next()) {
-  //     QJsonObject recordObject;
-  //     for(int i = 0; i < record.count(); i++) {
-  //       //recordObject.insert( query.record().fieldName(i),QJsonValue::fromVariant(query.value(i)) );
-  //     }
-  //
-  //     //recordsArray.push_back(recordObject);
-  //   }
-  //
-  //   //json.setArray(recordsArray);
-  // } else {
-  //   qDebug() << "La requete n'a pas abouti, la base de données est fermée !";
-  // }
+    QJsonDocument  json;
+    QJsonArray     recordsArray;
+
+    while(query.next()) {
+      QJsonObject recordObject;
+      for(int i = 0; i < record.count(); i++) {
+        recordObject.insert( query.record().fieldName(i),QJsonValue::fromVariant(query.value(i)) );
+      }
+
+      recordsArray.push_back(recordObject);
+    }
+
+    json.setArray(recordsArray);
+  } else {
+    qDebug() << "La requete n'a pas abouti, la base de données est fermée !";
+  }
 }
-  //return json.toJson();
+  retour = json.toJson();
   return retour;
 }
 //! [fonction public requete]
