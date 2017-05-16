186b78e43a2937e84ec14e41e87d952b8c773454
JeresB <boiselet.jeremy@gmail.com>
2017-05-16 11:59:52 +0200

    transfert de la connexion a la base de données du main au database.cpp

--- appli/database.cpp
+++ appli/database.cpp
@@ -3,8 +3,38 @@
 
 QT_USE_NAMESPACE
 
-//! [constructor]
+// [constructor]
 Database::Database(QObject *parent) : QObject(parent) {
-  
+  QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+
+  db.setHostName("localhost");
+  db.setUserName("livinglab");
+  db.setPassword("livinglab");
+  db.setDatabaseName("livinglab");
+
+  if(db.open()) {
+    qDebug() << "Vous êtes maintenant connecté à " << db.hostName();
+  } else {
+    qDebug() << "La connexion a échouée, désolé";
+  }
 }
 //! [constructor]
+
+// [fonction public requete]
+int Database::requete(QString requete) {
+  if(query.exec(requete)) {
+    rec = query.record();
+    qDebug() << "Selection réussi :)";
+    //int nameCol = rec.indexOf("ip_chambre"); // index of the field "name"
+    while(query.next()) {
+      qDebug() << "Nouvelle entrée";
+      for(int x=0; x < rec.count(); x++) {
+        //qDebug() << query.value(nameCol).toString(); // output all names
+        qDebug() << rec.fieldName(x) << " = " << query.value(x);
+      }
+    }
+  } else {
+    qDebug() << "Une erreur s'est produite. :(";
+  }
+}
+//! [fonction public requete]
